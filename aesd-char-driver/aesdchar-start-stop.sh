44f737881bd5e6d4c426813e2bb47c0b2cdca25c
Sriram-Y <sriramy2001@gmail.com>
2024-10-27 22:06:34 +0000

    changed aesdchar_load and aesdchar_unload paths

--- aesd-char-driver/aesdchar-start-stop.sh
+++ aesd-char-driver/aesdchar-start-stop.sh
@@ -2,10 +2,10 @@
 
 if [ "$1" = "start" ]; then
     echo "loading aesd-char-driver!"
-    start-stop-daemon -S -n aesdchar_load -a /usr/bin/aesdchar_load -- -d
+    start-stop-daemon -S -n aesdchar_load -a /bin/aesdchar_load -- -d
 elif [ "$1" = "stop" ]; then
     echo "unload aesd-char-driver!"
-    start-stop-daemon -K -n aesdchar_unload -a /usr/bin/aesdchar_unload -- -d
+    start-stop-daemon -K -n aesdchar_unload -a /bin/aesdchar_unload -- -d
 else
     echo "Usage: $0 command <start|stop>"
     exit 1
