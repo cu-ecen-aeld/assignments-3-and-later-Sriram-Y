f9b18edd48ca27f4325b8ea57d3fb4249f674d1b
Sriram-Y <sriramy2001@gmail.com>
2024-10-29 02:34:12 +0000

    changed message in start stop script aesdchar

--- aesd-char-driver/aesdchar-start-stop.sh
+++ aesd-char-driver/aesdchar-start-stop.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 if [ "$1" = "start" ]; then
-    echo "loading aesdsocket server!!!"
+    echo "loading aesdchardriver server!!!"
     aesdchar_load
 elif [ "$1" = "stop" ]; then
-    echo "unloading aesdsocket server!!!"
+    echo "unloading aesdchardriver server!!!"
     aesdchar_unload
 else
     echo "Usage: $0 <start|stop>"
