9e017f0834408635784181630373cd6fe84064bd
Sriram-Y <sriramy2001@gmail.com>
2024-10-30 05:15:41 +0000

    edits to start stop aesdsocket

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -3,6 +3,12 @@
 if [ "$1" = "start" ]; then
     echo "starting aesdsocket server!"
     start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
+    
+    if [ $? -eq 0 ]; then
+        echo "aesdsocket server started successfully!"
+    else
+        echo "Failed to start aesdsocket server."
+    fi
 elif [ "$1" = "stop" ]; then
     echo "stopping aesdsocket server!"
     start-stop-daemon -K -n aesdsocket --signal TERM
